01b26aaad68113436181ac38abf94b127505d288
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 19:20:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function goToSite(){
-let input = document.getElementById("urlInput").value;
-if(!input.startsWith("http")){
-input="https://"+input;
-}
-window.open(input);
-}
-
-function scrollToSection(id){
-document.getElementById(id).scrollIntoView({behavior:"smooth"});
-}
-
-function register(){
-alert("Đăng ký thành công");
-}
+console.log("Ready");
