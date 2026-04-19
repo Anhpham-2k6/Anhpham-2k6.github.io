319a5b419756969b6fc98591e24a0a8fc16a2203
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 19:35:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-// scroll
 function scrollToSection(id){
 document.getElementById(id).scrollIntoView({behavior:"smooth"});
 }
-
-function register(){
-alert("Đăng ký thành công!");
-}
-
-// galaxy
-const canvas=document.getElementById("bg");
-const ctx=canvas.getContext("2d");
-
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-
-let stars=[];
-for(let i=0;i<100;i++){
-stars.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-r:Math.random()*2
-});
-}
-
-function draw(){
-ctx.clearRect(0,0,canvas.width,canvas.height);
-stars.forEach(s=>{
-ctx.beginPath();
-ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
-ctx.fillStyle="white";
-ctx.fill();
-});
-requestAnimationFrame(draw);
-}
-draw();
