0e9d1c0c0fd245e8e26897b8d85f85b14b177b33
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 17:48:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ window.open("https://forms.gle/", "_blank");
 function contact(){
 alert("Email: smartdata@gmail.com");
 }
+
+/* SCROLL ANIMATION */
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+sections.forEach(sec => {
+const top = sec.getBoundingClientRect().top;
+if(top < window.innerHeight - 100){
+sec.classList.add('show');
+}
+});
+});
