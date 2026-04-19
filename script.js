42a531996a25c6bb35626cfccf94f68585328b0a
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 19:39:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function scrollToSection(id){
-document.getElementById(id).scrollIntoView({behavior:"smooth"});
+// scroll animation
+const elements = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+elements.forEach(el=>{
+const top = el.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+el.classList.add("show");
 }
+});
+});
