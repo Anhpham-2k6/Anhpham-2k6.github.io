0fa6ff39e841a09c84d85e166f07fce9aca934ef
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 18:50:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function goToSite(){
-    let input = document.getElementById("urlInput").value;
+let input = document.getElementById("urlInput").value;
+if(!input.startsWith("http")){
+input = "https://" + input;
+}
+window.open(input);
+}
 
-    if(!input.startsWith("http")){
-        input = "https://" + input;
-    }
-
-    window.open(input, "_blank");
+function scrollToSection(id){
+document.getElementById(id).scrollIntoView({behavior:"smooth"});
 }
 
 function register(){
-    alert("Đăng ký thành công!");
+alert("Đăng ký thành công!");
+}
+
+function contact(){
+alert("Liên hệ: smartdata@gmail.com");
 }
