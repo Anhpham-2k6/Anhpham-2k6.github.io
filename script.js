6a5931f6068a6f78f68af9b54fb720fedde9c486
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 18:14:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,9 @@ document.getElementById(id).scrollIntoView({behavior:'smooth'});
 }
 
 function register(){
-window.open("https://forms.gle/", "_blank");
+alert("Đăng ký thành công!");
 }
 
 function contact(){
-alert("Email: smartdata@gmail.com");
-}
-
-/* SCROLL */
-window.addEventListener("scroll",()=>{
-document.querySelectorAll(".reveal").forEach(el=>{
-if(el.getBoundingClientRect().top < window.innerHeight-100){
-el.classList.add("show");
-}
-});
-});
-
-/* CLICK EFFECT */
-function explodeEffect(el){
-el.innerText="💥";
-setTimeout(()=>{
-el.innerText="🤖";
-},500);
+alert("Liên hệ: smartdata@gmail.com");
 }
