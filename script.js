03f1f88b36556bcf7a73c841cac7ca651baa4d0a
Anhpham-2k6 <anhpham20022006@gmail.com>
2026-04-19 17:34:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,5 @@ window.open("https://forms.gle/", "_blank");
 }
 
 function contact(){
-alert("Email: smartdata@gmail.com");
-}
-
-/* ICON BACKGROUND */
-const icons = ["💻","📊","🤖","⚡","🧠","📁"];
-
-for(let i=0;i<30;i++){
-let span=document.createElement("span");
-span.innerText=icons[Math.floor(Math.random()*icons.length)];
-span.style.left=Math.random()*100+"%";
-span.style.top=Math.random()*100+"%";
-
-span.onclick=()=>{
-span.style.transform="scale(2)";
-setTimeout(()=>span.remove(),300);
-}
-
-document.getElementById("bg-icons").appendChild(span);
+alert("Liên hệ: smartdata@gmail.com");
 }
